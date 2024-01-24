df67f64e9382f952760a2681232be9c9c3502d72
simbados <22945705+simbados@users.noreply.github.com>
2024-01-24 20:52:32 +0100

    [go] add quicksort algo

--- go/75SortColors.go
+++ go/75SortColors.go
@@ -15,3 +15,39 @@ func bubbleSort(nums []int) {
 		}
 	}
 }
+
+// Quicksort
+func quickSort(nums []int) {
+	quickSortRec(nums, 0, len(nums)-1)
+}
+
+func quickSortRec(nums []int, low int, high int) {
+	if low >= 0 && high >= 0 && high > low {
+		pivot := partition(nums, low, high)
+		quickSortRec(nums, 0, pivot)
+		quickSortRec(nums, pivot+1, high)
+	}
+}
+
+func partition(nums []int, low int, high int) int {
+	pivot := nums[(low+high)/2]
+	i := low
+	j := high
+	for i <= j {
+		for nums[i] < pivot {
+			i = i + 1
+		}
+		for nums[j] > pivot {
+			j = j - 1
+		}
+		if i >= j {
+			return j
+		}
+		buffer := nums[i]
+		nums[i] = nums[j]
+		nums[j] = buffer
+		i++
+		j--
+	}
+	return j
+}
--- go/75SortColors_test.go
+++ go/75SortColors_test.go
@@ -16,6 +16,7 @@ func testData() []struct {
 		{[]int{1, 2, 5, 3, 2}, []int{1, 2, 2, 3, 5}},
 		{[]int{3, 2, 2, 3}, []int{2, 2, 3, 3}},
 		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, []int{0, 0, 1, 2, 2, 2, 3, 4}},
+		{[]int{3, 4, 3, 2}, []int{2, 3, 3, 4}},
 	}
 }
 
@@ -24,6 +25,17 @@ func TestBubbleSort(t *testing.T) {
 	for _, tt := range tests {
 		clone := slices.Clone(tt.a)
 		bubbleSort(tt.a)
+		if !slices.Equal(tt.a, tt.expected) {
+			t.Errorf("sortColors(%v) = %v; but got %v", clone, tt.expected, tt.a)
+		}
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	tests := testData()
+	for _, tt := range tests {
+		clone := slices.Clone(tt.a)
+		quickSort(tt.a)
 		if !slices.Equal(tt.a, tt.expected) {
 			t.Errorf("sortColors(%v) = %v; but got %v", clone, tt.expected, tt.a)
 		}
