2632e9a1d8aaa749a3bed39301163320853807ad
simbados <22945705+simbados@users.noreply.github.com>
2024-01-28 12:38:55 +0100

    [go] challenge 155

---
+++ go/155MinStack.go
@@ -0,0 +1,38 @@
+package main
+
+type MinStack struct {
+	arr []int
+	min []int
+}
+
+func Constructor() MinStack {
+	stack := MinStack{}
+	return stack
+}
+
+func (stack *MinStack) Push(val int) {
+	stack.arr = append(stack.arr, val)
+	if len(stack.min) == 0 || stack.min[len(stack.min)-1] >= val {
+		stack.min = append(stack.min, val)
+	}
+}
+
+func (stack *MinStack) Pop() {
+	popElement := stack.arr[len(stack.arr)-1]
+	stack.arr = stack.arr[0 : len(stack.arr)-1]
+	if len(stack.min) > 0 && stack.min[len(stack.min)-1] == popElement {
+		stack.min = stack.min[0 : len(stack.min)-1]
+	}
+}
+
+func (stack *MinStack) Top() int {
+	return stack.arr[len(stack.arr)-1]
+}
+
+func (stack *MinStack) GetMin() int {
+	return stack.min[len(stack.min)-1]
+}
+
+func (stack *MinStack) Len() int {
+	return len(stack.arr)
+}
---
+++ go/155MinStack_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMinStack(t *testing.T) {
+	a := Constructor()
+	a.Push(1)
+	a.Push(2)
+
+	res := a.GetMin()
+	if res != 1 {
+		t.Errorf("MinStack GetMin() = %v; but got %v", 1, res)
+	}
+	a.Pop()
+	res = a.Top()
+	if res != 1 {
+		t.Errorf("MinStack Top() = %v; but got %v", 1, res)
+	}
+	a.Pop()
+	if a.Len() != 0 {
+		t.Errorf("MinStack Pop = %v; but got %v", 2, res)
+	}
+}
