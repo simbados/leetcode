f2906cd2d40813c1797f72159a68dbfff8476449
simbados <22945705+simbados@users.noreply.github.com>
2024-07-29 20:36:15 +0200

    formating and adjust skeleton of tests

--- go/ReverseVowelOfAString.go
+++ go/ReverseVowelOfAString.go
@@ -3,31 +3,31 @@ package main
 import "strings"
 
 func reverseVowels(s string) string {
-    start, end := 0, len(s) - 1
-    stringAsRune := make([]rune, len(s))
-    for i, r := range s {
-        stringAsRune[i] = r
-    }
-    for start < end {
-        if !isVowel(stringAsRune[start]) {
-            start++
-            continue
-        } 
-        if !isVowel(stringAsRune[end]) {
-            end--
-            continue
-        }
-        stringAsRune[start], stringAsRune[end] = stringAsRune[end], stringAsRune[start]
-        start++
-        end--
-    }
-    return string(stringAsRune)
+	start, end := 0, len(s)-1
+	stringAsRune := make([]rune, len(s))
+	for i, r := range s {
+		stringAsRune[i] = r
+	}
+	for start < end {
+		if !isVowel(stringAsRune[start]) {
+			start++
+			continue
+		}
+		if !isVowel(stringAsRune[end]) {
+			end--
+			continue
+		}
+		stringAsRune[start], stringAsRune[end] = stringAsRune[end], stringAsRune[start]
+		start++
+		end--
+	}
+	return string(stringAsRune)
 }
 
 func isVowel(s rune) bool {
-    toCheck := strings.ToLower(string(s)) 
-    if toCheck == "a" || toCheck == "e" || toCheck == "i" || toCheck == "o" || toCheck == "u" {
-        return true
-    }
-    return false
+	toCheck := strings.ToLower(string(s))
+	if toCheck == "a" || toCheck == "e" || toCheck == "i" || toCheck == "o" || toCheck == "u" {
+		return true
+	}
+	return false
 }
--- go/kidsWithCandies.go
+++ go/kidsWithCandies.go
@@ -5,15 +5,15 @@ import (
 )
 
 func kidsWithCandies(candies []int, extraCandies int) []bool {
-    boolinger := make([]bool, len(candies))
-    if len(candies) == 0 {
-        return boolinger
-    }
-    sorted := slices.Clone(candies)
-    slices.Sort(sorted)
-    highest := sorted[len(sorted) - 1]
-    for index, value := range(candies) {
-        boolinger[index] = value + extraCandies >= highest
-    }
-    return boolinger
+	boolinger := make([]bool, len(candies))
+	if len(candies) == 0 {
+		return boolinger
+	}
+	sorted := slices.Clone(candies)
+	slices.Sort(sorted)
+	highest := sorted[len(sorted)-1]
+	for index, value := range candies {
+		boolinger[index] = value+extraCandies >= highest
+	}
+	return boolinger
 }
