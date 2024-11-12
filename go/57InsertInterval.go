82b73f2e8061762e0ceb18552a87f6f949743f92
simbados <22945705+simbados@users.noreply.github.com>
2024-11-12 20:28:27 +0100

    refactor: remove print and rename copy

--- go/57InsertInterval.go
+++ go/57InsertInterval.go
@@ -1,30 +1,27 @@
 package main
 
-import "fmt"
-
 func insertInterval(intervals [][]int, newInterval []int) [][]int {
 	if len(intervals) == 0 {
 		return [][]int{newInterval}
 	}
-	copy := append([][]int{newInterval}, intervals...)
-  fmt.Println(copy)
-	for i := 1; i < len(copy); i++ {
-		currentVal := copy[i]
-		previousVal := copy[i-1]
+	copyInterval := append([][]int{newInterval}, intervals...)
+	for i := 1; i < len(copyInterval); i++ {
+		currentVal := copyInterval[i]
+		previousVal := copyInterval[i-1]
 		if (previousVal[0] >= currentVal[0] && previousVal[0] <= currentVal[1]) ||
-			(previousVal[1] >= currentVal[0] && previousVal[1] <= currentVal[1]) || 
-      (previousVal[0] <= currentVal[0] && previousVal[1] >= currentVal[1]) {
+			(previousVal[1] >= currentVal[0] && previousVal[1] <= currentVal[1]) ||
+			(previousVal[0] <= currentVal[0] && previousVal[1] >= currentVal[1]) {
 			newValue := []int{min(previousVal[0], currentVal[0]), max(previousVal[1], currentVal[1])}
-			copy[i] = newValue
-			copy = removeIndex(i-1, copy)
-      i--
-		} else if (currentVal[1] < previousVal[1]) {
-      cache := copy[i-1]
-      copy[i-1] = copy[i]
-      copy[i] = cache
-    }
+			copyInterval[i] = newValue
+			copyInterval = removeIndex(i-1, copyInterval)
+			i--
+		} else if currentVal[1] < previousVal[1] {
+			cache := copyInterval[i-1]
+			copyInterval[i-1] = copyInterval[i]
+			copyInterval[i] = cache
+		}
 	}
-	return copy
+	return copyInterval
 }
 
 func removeIndex(index int, array [][]int) [][]int {
